e045b374aff46c4d4d79634e8015188224417120
asathiya007 <akshay.sathiya@gmail.com>
2020-03-26 21:18:50 -0400

    built first iteration of front end with Vue, built navbar

---
+++ client/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <b-navbar id="nav" class="mb2" toggleable="lg" type="light">
+      <b-navbar-brand class="f2" href="/">Taskboard</b-navbar-brand>
+      <div class="ml-auto">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/taskboard">Taskboard</router-link>
+      </div>
+    </b-navbar>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+  background: #42b883;
+}
+
+#nav a {
+  font-weight: bold;
+  color: white;
+  margin-right: 20px;
+  margin-left: 20px;
+}
+
+#nav a.router-link-exact-active {
+  color: darkgreen;
+}
+</style>
